add34d489942db420649d93dcec75156a5895c55
Jianzhang Ni <550912245@qq.com>
2023-04-24 16:58:44 +0800

    Create cuda_fedora.sh

---
+++ cuda_fedora.sh
@@ -0,0 +1,5 @@
+# https://fedoraproject.org/wiki/Cuda#Install_CUDA_Toolkit
+## follow the instructions from the previous link
+
+# Install the following perl dependencies before installing cuda
+sudo dnf install perl-Term-ReadLine-Gnu perl-Env perl-Tie-File 
